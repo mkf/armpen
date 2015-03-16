8517d1279c0c875825dde1324afe261c89e49af3
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-03-16 23:41:26 +0100

    Development

--- dummyarm/soft.py
+++ dummyarm/soft.py
@@ -1,21 +1,39 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ArchieT'
 class soft:
-	def __init__(self):
+	def __init__(self,sr,sph):
 		self.arc = arc
+		self.proste = proste
+	def posmodif(self,sr,sph):
+		self.sr = sr
+		self.sph = sph
 class arc:
-	def __init__(self,l,t,azim,x,y,a="deg"):
+	def __init__(self,l,tz,azim,sr,sph,ta="deg",spha="deg",azima="deg"):
 		import math
 		self.l = l
-		self.deg = deg = t if a=="deg" else math.degrees(t) if a=="rad" else None
-		self.rad = rad = t if a=="rad" else math.radians(t) if a=="deg" else None
-		self.pera = pera
-		self.perl = perl
-		self.r = float(l)/float(rad) if t!=0 else None
+		self.sr = sr
+		self.sph = sph
+		self.t = t = abs(tz)
+		self.tz = tz
+		self.clkw=clkw=math.s
+		self.ta = ta
+		self.spha = spha
+		self.azim = azim
+		self.azima=azima
+		for aexampel in (ta,spha,azima): assert aexampel=="deg" or aexampel=="rad"
+		degOLrad = lambda w: lambda x,a: x if a==w else (math.degrees(x) if w=="deg" else math.radians(x) if w=="rad" else None) if a==("deg" if w=="rad" else "rad" if w=="deg" else None) else None
+		self.deg=deg=degOLrad("deg")(t,ta)
+		self.rad=rad=degOLrad("rad")(t,ta)
+		self.sphdeg=sphdeg=degOLrad("deg")(sph,spha)
+		self.sphrad=sphrad=degOLrad("rad")(sph,spha)
+		self.azimdeg=azimdeg=degOLrad("deg")(azim,azima)
+		self.azimrad=azimrad=degOLrad("rad")(azim,azima)
+		self.czyproste=czyproste=t==0
+		self.r=r = float(l)/float(rad) if not czyproste else float('inf')
 
 	def draw(self):
 		pass
 class proste(arc):
-	def __init__(self,l,taz,x,y):
-		arc.__init__(self,l,0,taz,x,y)
-		#self.
+	def __init__(self,l,azim,sr,sph,spha="deg",azima="deg"):
+		arc.__init__(self,l,0,azim,sr,sph,spha=spha,azima=azima)
+		self.r=r=float('inf')
