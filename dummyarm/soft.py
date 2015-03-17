4a96cd69eab68240ea38bbe2ab2e81aee0ede86a
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-03-17 01:22:19 +0100

    Work in progress

--- dummyarm/soft.py
+++ dummyarm/soft.py
@@ -2,20 +2,34 @@
 __author__ = 'ArchieT'
 class soft:
 	def __init__(self,sr,sph):
+		self.bezier = bezier
 		self.arc = arc
 		self.proste = proste
-	def posmodif(self,sr,sph):
 		self.sr = sr
 		self.sph = sph
+	def posm(self,sr,sph):
+		self.sr = sr
+		self.sph = sph
+class polar:
+	"""Kartezjańskie na biegunowe"""
+	def __init__(self): pass
+	@staticmethod
+	def fkartz(x,y): import math ; return {'phi':math.atan2(x,y),'r':math.sqrt((x^2)+(y^2))}
+class bezier:
+	"""Krzywa Beziera"""
+	def __init__(self,sph,sr,c1ph,c1r,c2ph,c2r,eph,er):
+		pass
 class arc:
+	"""Łuk"""
 	def __init__(self,l,tz,azim,sr,sph,ta="deg",spha="deg",azima="deg"):
 		import math
+		import numpy
 		self.l = l
 		self.sr = sr
 		self.sph = sph
 		self.t = t = abs(tz)
 		self.tz = tz
-		self.clkw=clkw=math.s
+		self.clkw=clkw=float(numpy.sign(tz))
 		self.ta = ta
 		self.spha = spha
 		self.azim = azim
@@ -30,10 +44,20 @@ class arc:
 		self.azimrad=azimrad=degOLrad("rad")(azim,azima)
 		self.czyproste=czyproste=t==0
 		self.r=r = float(l)/float(rad) if not czyproste else float('inf')
-
 	def draw(self):
 		pass
 class proste(arc):
+	"""Prosta jako odmiana łuku"""
 	def __init__(self,l,azim,sr,sph,spha="deg",azima="deg"):
 		arc.__init__(self,l,0,azim,sr,sph,spha=spha,azima=azima)
 		self.r=r=float('inf')
+class punkt:
+	def __init__(self,r,ph,pha="deg"):
+		import math
+		degOLrad = lambda w: lambda x,a: x if a==w else (math.degrees(x) if w=="deg" else math.radians(x) if w=="rad" else None) if a==("deg" if w=="rad" else "rad" if w=="deg" else None) else None
+		self.r=r
+		self.ph=ph
+		self.phdeg=phdeg=degOLrad("deg")(ph,pha)
+		self.phrad=phrad=degOLrad("rad")(ph,pha)
+	def draw(self):
+		pass
