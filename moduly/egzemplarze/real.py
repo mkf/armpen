6c3cb76b661f2aa2a7bffa2b2057a0577fba0992
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-04-10 16:39:00 +0200

    Prace nad programem

--- moduly/egzemplarze/real.py
+++ moduly/egzemplarze/real.py
@@ -17,14 +17,20 @@ class real(maszyna):
 		betaprecision = kat(0.01,"deg")
 		self.alphaenginemultiplier = 10
 		self.betaenginemultiplier = 20
-
+		
+		#TODO: w domu zrobic to z with, bo to w sumie bedzie zapis do pliku ostatecznie - zrobic to z __enter__ i __exit__
+		#w sumie moze jednak nie
+		
 		maszyna.__init__(self,l1,l2,maxbetafromzero,minbetafromzero,alphaprecision,betaprecision)
-
-	def podnies_pioro(self): print "Podniesiono"
-	def opusc_pioro(self): print "Opuszczono"
-#	def gdziejestesmaszyno(self):
-#		from armpoz import gdzieramiona;
-#
-#		return gdzieramiona(kat(20,"deg"),kat(40,"deg"),self)  # dummy, to be replaced by real location
-	def movealpha(self,ruch): pass
-	def movebeta(self,ruch): pass
+		self.progfile = open('rysprog.py','w')
+		self.linprog('def main():',0)
+	def linprog(self,tresc,sintend): self.progfile.write(('    '*sintend)+tresc+"\n")
+	def podnies_pioro(self): 
+		self.linprog('#costam motor C podnies',1)
+		print "Podniesiono"
+	def opusc_pioro(self): 
+		self.linprog('#costam motor C opusc',1)
+		print "Opuszczono"
+	def movealpha(self,ruch): self.linprog('#costam motor A rusz o tyle',1)
+	def movebeta(self,ruch): self.linprog('#costam motor B rusz o tyle',1)
+	def syncedmove(self,a,b): self.linprog('#costam synced move motor A z synced motor B',1)
