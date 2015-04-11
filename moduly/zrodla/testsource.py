ebbc4933a8bdd67035be0dd7ebe4b7a7b0acd6b6
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-04-11 13:46:27 +0200

    Added a test source

---
+++ moduly/zrodla/testsource.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from moduly.obiekty import rysowania
+from moduly.wartosci.pos import pos
+
+class testsource:
+	def __init__(self): pass
+	def daj(self):
+		i = 1
+		while i<10:
+			yield rysowania.prosta(pos({'x':float(i)/float(10),'y':0.1}), pos({'x':float(i)/float(10),'y':0.7}))
+
