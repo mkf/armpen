98ced45c554bcbd0aff8bf049d2f961294c83cf8
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-12 00:18:51 +0200

    Working on tests

--- testscripts/basictest.py
+++ testscripts/basictest.py
@@ -2,5 +2,6 @@
 __author__ = 'ArchieT'
 from moduly.egzemplarze.fake import fake
 from moduly.zrodla.testsource import testsource
-with testsource('blah') as src:
-	for i in src.daj(): print i
+with fake() as arm:
+	with testsource('blah') as src:
+		for i in src.daj(): i.draw(arm,0.01)
