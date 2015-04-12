eb05df759d83474df6702e48035a3251d0fa7be6
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-12 11:15:25 +0200

    Working on #3, added traceback printing in testsource

--- moduly/zrodla/testsource.py
+++ moduly/zrodla/testsource.py
@@ -5,7 +5,11 @@ from moduly.wartosci.pos import pos
 class testsource:
 	def __init__(self,co): self.co = co
 	def __enter__(self): print "start",self.co ; return self
-	def __exit__(self, exc_type, exc_val, exc_tb): print "bye",self.co
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		print "bye",self.co
+		print "testsource.__exit__",exc_type,exc_val,exc_tb
+		import traceback
+		traceback.print_exception(exc_type,exc_val,exc_tb)
 	def daj(self):
 		i = 1
 		while i<10:
