d4e395e84c4bb598dbd1d4dbe750990460f5fadc
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-12 16:27:00 +0200

    Working on #3: debugging armpoz

--- moduly/arm/armpoz.py
+++ moduly/arm/armpoz.py
@@ -20,8 +20,9 @@ class armpoz(MovingMixIn,pos):
 		self.arm = arm
 		wyprost = poz.r==arm.l1+arm.l2
 		assert arm.l1+arm.l2>poz.r or wyprost, "Nie starcza ramion"
+		assert arm.l1-arm.l2<poz.r, "Za blisko!"
 		print "l1",arm.l1,'l2',arm.l2,'r',self.r  #debug
-		cosalphaodr = (arm.l1/(2*self.r))+(self.r/(2*arm.l1))-(arm.l2*arm.l2/(2*self.r*arm.l1))
+		cosalphaodr = (arm.l1/(2*self.r))+(self.r/(2*arm.l1))-(arm.l2*arm.l2/(2*self.r*arm.l1))  # dla przykładowych wyszło ~1.04 — ale to chyba było za blisko po prostu
 		cosbeta = (arm.l1/(2*arm.l2))+(arm.l2/(2*arm.l1))-(self.r*self.r/(2*arm.l1*arm.l2))
 		self.alphaodr = arctrig(cosalphaodr,'cos')
 		self.beta = arctrig(cosbeta,'cos')
