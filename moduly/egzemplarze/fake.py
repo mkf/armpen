160e391aa7454c51a08a6d02b4e9c62003e42141
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-12 10:19:32 +0200

    Added traceback handling in fake.__exit__

--- moduly/egzemplarze/fake.py
+++ moduly/egzemplarze/fake.py
@@ -29,7 +29,9 @@ class fake(maszyna):
 
 		return self
 	def __exit__(self, exc_type, exc_val, exc_tb):
-		print "__exit__",exc_type,exc_val,exc_tb
+		print "fake.__exit__",exc_type,exc_val,exc_tb
+		import traceback
+		traceback.print_exception(exc_type,exc_val,exc_tb)
 		while True:
 			for event in pygame.event.get():
 				if event.type!=pygame.MOUSEMOTION: print event
