c38e8d35e47d0ec1931fbf14d7dc9bb221fc9f6d
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-12 14:41:29 +0200

    Working on #3, debugging pos

---
+++ postest.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ArchieT'
+from moduly.wartosci.pos import pos
+ap = pos({'x':2,'y':3})
+print "ap",ap
+print "str(ap)",str(ap)
+print "dict(ap)",dict(ap)
+print "str(dict(ap))",str(dict(ap))
