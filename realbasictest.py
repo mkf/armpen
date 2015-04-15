41ffe22979fc652dc947ea30e7bad5ca0ad5ff1f
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-15 17:03:23 +0200

    Added realbasictest.py

---
+++ realbasictest.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ArchieT'
+from moduly.egzemplarze.real import real
+from moduly.zrodla.testsource import testsource
+with real() as arm:
+	with testsource('blah') as src:
+		for i in src.daj():
+			i.draw(arm,0.01)
+			print i,i.__dict__
