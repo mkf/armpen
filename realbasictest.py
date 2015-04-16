30af1055bba118c9862b45962787b73b0881182d
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-16 08:11:31 +0200

    Larger steps in tests

--- realbasictest.py
+++ realbasictest.py
@@ -5,5 +5,5 @@ from moduly.zrodla.testsource import testsource
 with real() as arm:
 	with testsource('blah') as src:
 		for i in src.daj():
-			i.draw(arm,0.01)
+			i.draw(arm,0.1)
 			print i,i.__dict__
