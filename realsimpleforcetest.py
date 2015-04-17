dd58bf962384f861d56f8ecd1e77230302c78f8d
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-17 18:10:28 +0200

    Added forced relative move, no more absolute position afterwards

--- realsimpleforcetest.py
+++ realsimpleforcetest.py
@@ -3,5 +3,6 @@ __author__ = 'ArchieT'
 from moduly.egzemplarze.real import real
 from moduly.wartosci.kat import kat
 with real() as arm:
-	arm.chamskonasilnik(kat(-90,"deg"),kat(-90,"deg"))
+	#arm.chamskonasilnik(kat(-90,"deg"),kat(-90,"deg"))
 	#arm.chamskonasilnik(kat(20,"deg"),kat(30,"deg"))
+	arm.chamskonasilnik(beta=kat(-90,"deg"))
