da4013ddaf9f867ed3c8fd63fcde57e2fac0e72a
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-17 15:28:13 +0200

    Working on rysowania.krzywa: rethink-rewrite

--- moduly/obiekty/rysowania.py
+++ moduly/obiekty/rysowania.py
@@ -24,8 +24,10 @@ class punkt(rysunek):
 class krzywa(rysunek):
 	def __init__(self,start,funkcjadefiniujaca):
 		rysunek.__init__(self,start);self.funkcjadefiniujaca=funkcjadefiniujaca
+		self.absstopstepprog = self.absolutniestopdlastepprog = 3000
+		assert 'czyend' in dir(self)
 	def draw(self,ramie,step):
-		absolutniestopdlastepprog = 3000
+		absstopstepprog = absolutniestopdlastepprog = self.absstopstepprog
 		bylprob=False;juzstepprob=0;doprzemend=False
 		self.funkcja=funkcja=self.funkcjadefiniujaca(ramie,juzstepprob)
 		from moduly.arm.maszyna import nasilnik
@@ -34,11 +36,11 @@ class krzywa(rysunek):
 		try:
 			if not bylprob: doprzem = armpoz(self.start,ramie)
 			else:
-				print juzstepprob  #debug
+				print juzstepprob
 				doprzemfun = funkcja(juzstepprob)
-				print doprzemfun  #debug
-				doprzemend = doprzemfun['e']
 				doprzem = doprzemfun['w']
+				print doprzemfun
+				doprzemend = doprzemfun['e']
 			if not doprzemend:
 				doprzem.przemiesc()
 				ramie.opusc_pioro()
@@ -47,46 +49,47 @@ class krzywa(rysunek):
 				print "ZROBILIŚMY KRZYWĄ"
 			else: print doprzemend,doprzemfun,doprzem
 		except AssertionError:
-			bylprob = True
-			if not doprzemend:
-				juzstepprob+=probstep
-				print ramie,step,bylprob,juzstepprob,doprzemend    #debug
-				#self.draw(ramie,step,bylprob,juzstepprob,doprzemend)
-				zrobione = False
-				while not doprzemend and not zrobione and juzstepprob<=absolutniestopdlastepprog:
-					doprzemend = self.czyend(juzstepprob)
-					print "tryin",ramie,step,bylprob,juzstepprob,doprzemend,self
-					try:
-						doprzemfun = funkcja(juzstepprob)
-						doprzemend = doprzemfun['e']
-						doprzem = doprzemfun['w']
-						if not doprzemend:
-							doprzem.przemiesc()
-							ramie.opusc_pioro()
-							ruch = nasilnik(self.funkcja,doprzem,step,str(self))
-							ramie.dajnasilnik(ruch)
-							print "ZROBILIŚMY KRZYWĄ"
-						zrobione = True
-					except AssertionError:
-						if not doprzemend and not zrobione: juzstepprob+=probstep
-				if juzstepprob<=absolutniestopdlastepprog:
-					print "Nie zrobiono"
-
+			bylprob=True
+			juzstepprob+=probstep
+			doprzemend = self.czyend(juzstepprob)
+			print ramie,step,bylprob,juzstepprob,doprzemend    #debug
+			#self.draw(ramie,step,bylprob,juzstepprob,doprzemend)
+			zrobione = False
+			while not doprzemend and not zrobione and juzstepprob<absstopstepprog:
+				doprzemend = self.czyend(juzstepprob)
+				print "tryin",ramie,step,bylprob,juzstepprob,doprzemend,self
+				try:
+					doprzemfun = funkcja(juzstepprob)
+					doprzemend = doprzemfun['e']
+					doprzem = doprzemfun['w']
+					if not doprzemend:
+						doprzem.przemiesc()
+						ramie.opusc_pioro()
+						ruch = nasilnik(self.funkcjadefiniujaca(ramie,juzstepprob),doprzem,step,str(self))
+						ramie.dajnasilnik(ruch)
+						print "ZROBILIŚMY KRZYWĄ"
+					zrobione = True
+				except AssertionError:
+					if not doprzemend and not zrobione:
+						juzstepprob+=probstep
+						doprzemend = self.czyend(juzstepprob)
+			print "Nie zrobiono — limit absstopstepprog" if juzstepprob<=absstopstepprog else "koniec (przynajmniej nie osiągnięto limitu absstopstepprog)"
 		#except RuntimeError:
 		#	print "Mamy RuntimeError, na razie olewamy"
 
 class prosta(krzywa):     #      prosta(cubicbezier):
 	def __init__(self,start,end):   #cubicbezier.__init__(self,start,start,end,end)
 		ks = start.ka;ke = end.ka; dx=ke['x']-ks['x'] ; dy=ke['y']-ks['y']
-		funkdef = lambda arm,probst=0: lambda t: {'w':armpoz({'x':((t+probst)*dx)+ks['x'],'y':((t+probst)*dy)+ks['y']},arm),'e':t+probst>=1}
+		self.czyend = czyend = lambda x: x>=1
+		funkdef = lambda arm,probst=0: lambda t: {'w':armpoz({'x':((t+probst)*dx)+ks['x'],'y':((t+probst)*dy)+ks['y']},arm),'e':czyend(t+probst)}
 		krzywa.__init__(self,start,funkdef)
 		self.probstep = None
 		self.end=end
-		self.czyend = lambda x: x>=1
 
 class quadrbezier(krzywa):    # fragment paraboli
 	def __init__(self,start,c,end):
 		self.c=c;self.end=end;from armpoz import armpoz
+		self.czyend = czyend = lambda x: x>=1
 		adfunk = lambda u,t: ((1-t)*(1-t)*start.ka[u])+(2*(1-t)*t*c.ka[u])+(t*t*end.ka[u])
 		self.probstep = None
 		funkdef=lambda arm,probst=0:lambda t:{
@@ -94,22 +97,21 @@ class quadrbezier(krzywa):    # fragment paraboli
 				'x': adfunk('x',t+probst),
 				'y': adfunk('y',t+probst)
 			},arm),
-			'e':t+probst>=1
+			'e':czyend(t+probst)
 		}
 		krzywa.__init__(self,start,funkdef)
-		self.czyend = lambda x: x>=1
 
 class cubicbezier(krzywa):
 	def __init__(self,start,c1,c2,end):
 		adfunk = lambda u,t: ((1-t)*(1-t)*(1-t)*start.ka[u])+(3*(1-t)*(1-t)*t*c1.ka[u])+(3*(1-t)*t*t*c2.ka[u])+(t*t*t*end.ka[u])
 		self.probstep = None
+		self.czyend = czyend = lambda x: x>=1
 		funkcjadefiniujaca = lambda arm,probst=0: lambda t: {'w':armpoz({
 			'x':adfunk('x',t+probst),
 			'y':adfunk('y',t+probst)
-		},arm),'e':t+probst>=1}
+		},arm),'e':czyend(t+probst)}
 		krzywa.__init__(self,start,funkcjadefiniujaca)
 		self.c1=c1;self.c2=c2;self.end=end
-		self.czyend = lambda x: x>=1
 
 class plotxy(krzywa):
 	def __init__(self,fFromX,zero,oneXzeroY,oneYzeroX): # one will be the maximum — you have to divide the values appropiately
@@ -119,6 +121,7 @@ class plotxy(krzywa):
 		#	'x':x,
 		#	'y':min(sorted([]))
 		#})}
+		self.czyend = czyend = lambda x: x>=1
 		def fdef(arm,probst=0):
 			def tocos(xv):
 				x = xv+probst
@@ -127,10 +130,9 @@ class plotxy(krzywa):
 				return {'w': armpoz({
 					'x':x,
 					'y':kolej[0],
-				},arm),'e':x>=1}
+				},arm),'e':czyend(x)}
 			return tocos
 		krzywa.__init__(self,zero,fdef)
-		self.czyend = lambda x: x>=1
 
 class plotrphi(krzywa):
 	def __init__(self,fFromPhi,zero,minR,oneR_onZeroPhi,minPhi,maxPhi):  # one is the maximum for radius
@@ -142,6 +144,7 @@ class plotrphi(krzywa):
 		onerzerophi_vect_PosPol = pos(onerzerophi_vect).po
 		rone = onerzerophi_vect_PosPol['r']
 		phione = onerzerophi_vect_PosPol['phi']
+		self.czyend = czyend = lambda x: x>=maxPhi.deg
 		def fdef(arm,probst=0):
 			probstk= kat(probst,'deg')
 			def tocos(phival):
@@ -151,10 +154,9 @@ class plotrphi(krzywa):
 				pwzs = pos({'phi':(phi+phione).naplaszczyznie['katnaplaszczyznie'],'r':kolej[0]*rone})
 				# pwzs — pozycja względem zera już przeskalowana
 				pjp = pwzs + vect  #pozycja już przesunięta
-				return {'w': armpoz(pjp,arm),'e':phi>=maxPhi}
+				return {'w': armpoz(pjp,arm),'e':czyend(phi)}
 			return tocos
 		krzywa.__init__(self,zero,fdef)
-		self.czyend = lambda x: x>=maxPhi.deg
 
 class plotrphiFromZero(krzywa):
 	def __init__(self,fFromPhi,minR,oneR_onZeroPhi,minPhi,maxPhi):
@@ -166,6 +168,7 @@ class plotrphiFromZero(krzywa):
 		onerzerophi_vect_PosPol = pos(onerzerophi_vect).po
 		rone = onerzerophi_vect_PosPol['r']
 		phione = onerzerophi_vect_PosPol['phi']
+		self.czyend = czyend =  lambda x: x>=maxPhi.deg
 		def fdef(arm,probst=0):
 			probstk = kat(probst,'deg')
 			def tocos(phival):
@@ -173,10 +176,9 @@ class plotrphiFromZero(krzywa):
 				kolej = [0,fFromPhi(phi),1];kolej.remove(min(kolej));kolej.remove(max(kolej))
 				#tymczasowo będzie jechało po ramce jak wartość poza zakresem, później się to zmieni
 				pjp = pos({'phi':(phi+phione).naplaszczyznie['katnaplaszczyznie'],'r':kolej[0]*rone})
-				return {'w': armpoz(pjp,arm),'e':phi>=maxPhi}
+				return {'w': armpoz(pjp,arm),'e':czyend(phi)}
 			return tocos
 		krzywa.__init__(self,zero,fdef)
-		self.czyend = lambda x: x>=maxPhi.deg
 
 class arcFromZero(krzywa):
 	def __init__(self,r,minPhi,maxPhi):
@@ -184,12 +186,12 @@ class arcFromZero(krzywa):
 		self.probstep = 0.0005
 		self.r=r;self.minPhi=minPhi;self.maxPhi=maxPhi
 		start = pos({'r':r,'phi':minPhi})
+		self.czyend = czyend = lambda x: x>=1
 		def fdef(arm,probst=0):
 			def tocos(xv):
 				x = xv+probst
-				return {'w': armpoz({'r':r,'phi':minPhi+((maxPhi-minPhi)*x)},arm),'e':x>=1}
+				return {'w': armpoz({'r':r,'phi':minPhi+((maxPhi-minPhi)*x)},arm),'e':czyend(x)}
 			return tocos
 		krzywa.__init__(self,start,fdef)
-		self.czyend = lambda x: x>=1
 #TODO: arcfromelbow
 #TODO: allipsefrag
