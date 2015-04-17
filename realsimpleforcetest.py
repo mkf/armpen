7c85472edbf680200d45f6dd2fb78d5e7b7326ba
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-17 18:05:59 +0200

    Added forced relative move, no more absolute position afterwards

---
+++ realsimpleforcetest.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ArchieT'
+from moduly.egzemplarze.real import real
+with real() as arm:
+	arm.chamskonasilnik(kat(10,"deg"),kat(10,"deg"))
+	arm.chamskonasilnik(kat(20,"deg"),kat(30,"deg"))
---
+++ simpleforcetest.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ArchieT'
+from moduly.egzemplarze.fake import fake
+with fake() as arm:
+	arm.chamskonasilnik(kat(10,"deg"),kat(10,"deg"))
+	arm.chamskonasilnik(kat(20,"deg"),kat(30,"deg"))
