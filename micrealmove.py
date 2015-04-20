19d99a9e33b37b0e9cf909a8e4b077d13060a4dc
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-20 11:04:08 +0200

    micrealmove

--- micrealmove.py
+++ micrealmove.py
@@ -6,7 +6,7 @@ from nxt.sensor import PORT_1,PORT_2,Touch,Sound
 blah = raw_input('Podłącz czujniki do 1,2 przy kostce alfa i wciśnij enter')
 am = float(raw_input('daj współczynnik alpha '))
 bm = float(raw_input('daj współczynnik beta '))
-assert am!=0 and bm!=0,str('%s,%s' % (str(am),str(bm)))
+assert am!=0 or bm!=0,str('%s,%s' % (str(am),str(bm)))
 with real() as arm:
 	touch = Touch(arm.stera,PORT_1)
 	sound = Sound(arm.stera,PORT_2)
